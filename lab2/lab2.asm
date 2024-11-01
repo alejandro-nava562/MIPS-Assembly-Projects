43c5ad1092d83c8918b397c6aaf4715511c0d492
alejandronava562 <navaalejandro562@gmail.com>
2024-10-31 21:24:58 -0700

    change case functionality added

--- lab2/lab2.asm
+++ lab2/lab2.asm
@@ -97,6 +97,41 @@ jr $ra
 # $a2: Size of array
 find_local_maxima:
 ############################ Part 2: your code begins here ###
+	sw $zero, 0($a1)
+	addi $t0, $a2, -1 # get the last element (N - 1 )
+	sll $t1, $t0, 2 # multiply by 4 to get the byte offset
+	sw $zero, 0($a1)
+	
+	# set up the loop
+	addi $t2, $a0, 4
+	addi $t3, $a1, 4
+	addi $t4, $a2, -2 # loop counter to (N - 2) so that we iterate from index 1 to N - 2
+	
+loop_find_local_maxima:
+	# 3 elements being loaded: current, previous, and next element
+	lw $t5, 0($t2)                  # $t5 = input[i]
+	lw $t6, -4($t2)                 # $t6 = input[i-1]
+	lw $t7, 4($t2)                  # $t7 = input[i+1]
+
+	# Check if current element is greater than both neighbors
+	ble $t5, $t6, not_max           # If input[i] <= input[i-1], not a local maximum
+	ble $t5, $t7, not_max           # If input[i] <= input[i+1], not a local maximum
+
+	# If it is a local maximum, set output[i] = 1
+	li $t8, 1
+	sw $t8, 0($t3)
+	j next
+
+not_max:
+	# Otherwise, set output[i] = 0
+	sw $zero, 0($t3)
+
+next:
+	# Move to the next elements
+	addi $t2, $t2, 4                # Move $t2 to input[i+1]
+	addi $t3, $t3, 4                # Move $t3 to output[i+1]
+	addi $t4, $t4, -1               # Decrement the loop counter
+	bgtz $t4, loop_find_local_maxima
 
 ############################ Part 2: your code ends here ###
 jr $ra
@@ -118,6 +153,42 @@ jr $ra
 ###############################################################
 change_case:
 ############################## Part 3: your code begins here ###
+    lb $t0, 0($a0)
 
+# Loop until we reach the null character
+loop_change_case:
+	beq $t0, $zero, end_loop  # If current character is null (0) we are done
+
+        # Check if character is uppercase (A-Z)
+	li $t1, 65                # ASCII value of 'A'
+	li $t2, 90                # ASCII value of 'Z'
+        blt $t0, $t1, check_lower # If char < 'A', check if it's lowercase
+        bgt $t0, $t2, check_lower # If char > 'Z', check if it's lowercase
+
+        # Convert uppercase to lowercase
+        addi $t0, $t0, 32         # Add 32 to convert to lowercase
+        j store_char              # Store the converted character
+
+check_lower:
+        # Check if character is lowercase (a-z)
+        li $t1, 97                # ASCII value of 'a'
+        li $t2, 122               # ASCII value of 'z'
+        blt $t0, $t1, next_char   # If char < 'a', skip to the next character
+        bgt $t0, $t2, next_char   # If char > 'z', skip to the next character
+
+        # Convert lowercase to uppercase
+        subi $t0, $t0, 32         # Subtract 32 to convert to uppercase
+
+store_char:
+        sb $t0, 0($a1)            # Store the character in the output string
+        addi $a1, $a1, 1          # Move to the next position in the output string
+
+next_char:
+        addi $a0, $a0, 1          # Move to the next character in the input string
+        lb $t0, 0($a0)            # Load the next character
+        j loop_change_case
+
+end_loop:
+        sb $zero, 0($a1)          # Append null character to the end of the output str
 ############################## Part 3: your code ends here ###
 jr $ra
